b7ba085d1321c2b274961b315bf82c259ab4f57e
Qusai Sherif <27321008@edu.cbe.ab.ca>
2024-10-28 15:28:23 -0600

    finished gamestate

--- server.js
+++ server.js
@@ -7,21 +7,23 @@ server.use(express.static("public"))
 //All your code goes here
 let activeSessions={}
 let randWord;
-async function check(){
-    let response = await fetch("https://random-word-api.vercel.app/api?words=1&length=5")
-    let results =await response.json()
-    randWord = results[0]
-}
+// async function check(){
+//     let response = await fetch("https://random-word-api.vercel.app/api?words=1&length=5")
+//     let results =await response.json()
+//     randWord = results[0]
+// }
 
 server.get("/newgame", function(req,res){
     let newID = uuid.v4()
     let newgame = {
         wordToguess: "chase",
+        guesses: [],
         wrongLetters: [],
         closeLetters: [],
         rightLetters:[],
         remainingGuesses:6,
-        gameOver :false
+        gameOver:false
+        
     }
     activeSessions[newID]= newgame
     res.status(201)
@@ -29,17 +31,26 @@ server.get("/newgame", function(req,res){
 })
 server.get('/gamestate',(req,res) => {
    let sessionID =  req.query.sessionID
-    ///////////////////////////////
-   res.send({sessionID: newID})
-    // if (req.query.sessionID== undefined) {
-    //     res.status(404)
-    //     }  
-    // if (req.query.sessionID == null) {
+ 
+    if (!sessionID) {
+        res.status(400).send({error: "id is missing"})
+    }else if(activeSessions[sessionID]){
+               res.status(200).send({gameState:activeSessions[sessionID]})
+    } else {
+    res.status(404).send({error:"Game doesn't exist"})
+
+    }
+//    res.send({sessionID: newID}}
+    // if (req.query.sessionID== undefined)}{
+    //     res.status(404}
+    //     } }
+    // if (req.query.sessionID == null) }
     //     res.status(400)
     // }  
     //     res.json.stringify(newgame.)
-    
+    }
 )
+
 
 
 //Do not remove this line. This allows the test suite to start
