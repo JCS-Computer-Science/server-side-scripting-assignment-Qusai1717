f34117fe753ead8bc4ccb1b5cb2de61259e21b58
Qusai Sherif <27321008@edu.cbe.ab.ca>
2024-10-29 13:57:26 -0600

    almost done POST endpoint

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ let randWord;
 //     randWord = results[0]
 // }
 
-server.get("/newgame", function(req,res){
+server.get("/newgame", (req,res) => {
     let newID = uuid.v4()
     let newgame = {
         wordToguess: "chase",
@@ -38,17 +38,48 @@ server.get('/gamestate',(req,res) => {
                res.status(200).send({gameState:activeSessions[sessionID]})
     } else {
     res.status(404).send({error:"Game doesn't exist"})
+}
+    }
+)
+server.get('/guess',(req,res) => {
+    let sessionID =req.body.sessionID
+    let userGuess = req.body.guess;
+    let session = activeSessions[sessionID]
+    let value = userGuess.split("").toString()
+    if (!sessionID) {
+        res.status(400).send({error: "Session ID is missing"})
+    } else if (value.length != 5) {
+        res.status(400).send({error: "Invalid Guess"})
+    } else if(activeSessions[sessionID]) {
 
+        let result;
+        
+         
+         let realValue = session.wordToguess.split("")
+    for (let i = 0; i <= value.length; i++) {
+       for (let j = 0; j <= 5; j++) {
+       
+        if (value[i] == realValue[i]) {
+          result = "RIGHT"
+        }else if(value[i] == realValue[i+j]){
+            result = "CLOSE"
+        }else{
+          result = "WRONG"
+          
+        }
     }
-//    res.send({sessionID: newID}}
-    // if (req.query.sessionID== undefined)}{
-    //     res.status(404}
-    //     } }
-    // if (req.query.sessionID == null) }
-    //     res.status(400)
-    // }  
-    //     res.json.stringify(newgame.)
+    
     }
+    let obj ={
+        value:value[i], result: result
+    }
+    session.guesses.push(obj)
+    res.status(201).send({gameState:activeSessions[sessionID]})
+
+     } else {
+         res.status(404).send({error: "Session doesn't exist"})
+    }
+}
 )
 
 
